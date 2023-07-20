dfc669e6b41b31d070c78fb45b927109fd30fbcf
abelkm99 <abelkidanemariam99@gmail.com>
2023-07-20 15:58:17 +0300

    test checking

--- afro-chat-backend/app/routers/api_v1/users.py
+++ afro-chat-backend/app/routers/api_v1/users.py
@@ -1,20 +1,23 @@
-from fastapi import APIRouter,Request,Depends
+from fastapi import APIRouter, Request, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
 from app.models.user import User
 
 users_router = APIRouter(
-            prefix="/items",
-            tags=["items"],
-            responses={404: {"description": "Not found"}},
-    )
+    prefix="/items",
+    tags=["items"],
+    responses={404: {"description": "Not found"}},
+)
 
 
 @users_router.get("/")
 async def read_items(
-        request:Request,
-        session : AsyncSession = Depends(get_db)
-    ):
+    request: Request,
+    session: AsyncSession = Depends(get_db)
+):
+    user = User(name="able")
+
+    await user.save(session)
 
     logger = request.state.logger
     logger.debug("this is debugger")
