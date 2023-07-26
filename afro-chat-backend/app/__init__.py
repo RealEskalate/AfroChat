ae56981233776f4e1c4d03fff9661bfcc172c78d
abelkm99 <abelkidanemariam99@gmail.com>
2023-07-26 10:13:20 +0300

    moved the statup and shutdown even listenr from app to main.py

--- afro-chat-backend/app/__init__.py
+++ afro-chat-backend/app/__init__.py
@@ -10,20 +10,10 @@ async def logger_middleware(request: Request, call_next):
     return response
 
 
-async def shutdown_event():
-    fast_api_logger.info("Shutting down...")
-
-
-async def startup_event():
-    fast_api_logger.info("Starting up...")
-
-
 def create_app(config: Config) -> FastAPI:
     print(config.CONFIG_TYPE)
     app = FastAPI()
     app.include_router(api_v1_router)
     app.middleware('http')(logger_middleware)
-    app.on_event('shutdown')(shutdown_event)
-    app.on_event('startup')(startup_event)
 
     return app
--- afro-chat-backend/main.py
+++ afro-chat-backend/main.py
@@ -2,17 +2,26 @@ import logging
 
 from config import initial_config as config
 from fastapi import Request
-from app import create_app
+from app import create_app, fast_api_logger
 
 app = create_app(config)
 
 
+@app.on_event('shutdown')
+async def shutdown_event():
+    fast_api_logger.info("Shutting down...")
+
+
+@app.on_event('startup')
+async def startup_event():
+    fast_api_logger.info("Starting up...")
+
+
 @app.get('/')
-async def home(request:Request):
+async def home(request: Request):
     logger = request.state.logger
     logger.debug("This is a debug message.")
     logger.info("This is an info message.")
     logger.warning("This is a warning message.")
     logger.error("This is an error message.")
-    return {"hello":"world"}
-
+    return {"hello": "world"}
