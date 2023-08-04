3ca88bda749eb29236433731c64cd9a827291ea0
abelkm99 <abelkidanemariam99@gmail.com>
2023-08-04 15:16:18 +0300

    changed the keyboards

--- afro-chat-backend/bot/features/persona/keyboards.py
+++ afro-chat-backend/bot/features/persona/keyboards.py
@@ -7,9 +7,8 @@ persona_cb = CallbackData('persona', 'name')
 persona_kb = InlineKeyboardMarkup(row_width=2)
 
 persona_kb.insert(InlineKeyboardButton(text="Albert Einstein", callback_data=persona_cb.new('albert')))
-persona_kb.insert(InlineKeyboardButton(text="Samiya", callback_data=persona_cb.new('sami')))
-persona_kb.insert(InlineKeyboardButton(text="Birook", callback_data=persona_cb.new('birook')))
-persona_kb.insert(InlineKeyboardButton(text="Eyu", callback_data=persona_cb.new('eyu')))
-persona_kb.insert(InlineKeyboardButton(text="Roba", callback_data=persona_cb.new('Roba')))
+persona_kb.insert(InlineKeyboardButton(text="Bill Gates", callback_data=persona_cb.new('sami')))
+persona_kb.insert(InlineKeyboardButton(text="Kevin Hart", callback_data=persona_cb.new('birook')))
+persona_kb.insert(InlineKeyboardButton(text="Nelson Mandela", callback_data=persona_cb.new('eyu')))
 persona_kb.row(InlineKeyboardButton(
     text='🔙 Go Back to Main Menu', callback_data='start'))
