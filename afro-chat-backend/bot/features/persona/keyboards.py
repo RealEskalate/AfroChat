49f4e19b79e93f8bd4e541ad65b40e96715088e9
abelkm99 <abelkidanemariam99@gmail.com>
2023-08-04 15:24:03 +0300

    miner fixes on keyboard

--- afro-chat-backend/bot/features/persona/keyboards.py
+++ afro-chat-backend/bot/features/persona/keyboards.py
@@ -6,9 +6,13 @@ persona_cb = CallbackData('persona', 'name')
 
 persona_kb = InlineKeyboardMarkup(row_width=2)
 
-persona_kb.insert(InlineKeyboardButton(text="Albert Einstein", callback_data=persona_cb.new('albert')))
-persona_kb.insert(InlineKeyboardButton(text="Bill Gates", callback_data=persona_cb.new('sami')))
-persona_kb.insert(InlineKeyboardButton(text="Kevin Hart", callback_data=persona_cb.new('birook')))
-persona_kb.insert(InlineKeyboardButton(text="Nelson Mandela", callback_data=persona_cb.new('eyu')))
+persona_kb.insert(InlineKeyboardButton(text="Albert Einstein",
+                  callback_data=persona_cb.new('albert')))
+persona_kb.insert(InlineKeyboardButton(text="Bill Gates",
+                  callback_data=persona_cb.new('sami')))
+persona_kb.insert(InlineKeyboardButton(text="Kevin Hart",
+                  callback_data=persona_cb.new('birook')))
+persona_kb.insert(InlineKeyboardButton(
+    text="Nelson Mandela", callback_data=persona_cb.new('Mandela')))
 persona_kb.row(InlineKeyboardButton(
     text='🔙 Go Back to Main Menu', callback_data='start'))
