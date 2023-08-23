b603799b99c2fce11ca9eb2f8b0694e9ba085830
abelkm99 <abelkidanemariam99@gmail.com>
2023-08-23 10:35:39 +0300

    Added Handlers for the tools.

--- afro-chat-backend/bot/features/tools/handlers.py
+++ afro-chat-backend/bot/features/tools/handlers.py
@@ -1,17 +1,55 @@
 from aiogram import types
+from bot.bot_state import State
+from bot.person_list import Persona, PersonaState
+
+from .keyboards import tools_kb
+from .texts import tools_text
+import time
 
 
 async def handle_tools_command(message: types.Message):
     try:
-        pass
-        return await message.answer("Tools Will be Added in the near future")
+        return await message.answer(text=tools_text, reply_markup=tools_kb)
     except Exception:
         return await message.answer(text="something happen please came back letter")
 
 
 async def handle_tools_callback(call: types.CallbackQuery):
     try:
-        pass
-        return await call.message.answer("Tools Will be Added in the near future")
+        return await call.message.answer(text=tools_text, reply_markup=tools_kb)
     except Exception:
-        return await call.message.answer(text="something happen please came back letter")
+        return await call.message.answer(
+            text="something happen please came back letter"
+        )
+
+
+async def handle_tool_click_callback(call: types.CallbackQuery):
+    try:
+        tool_name = call.data
+        chat_id = str(call.message.chat.id)
+        try:
+            tool: Persona = PersonaState[tool_name]
+
+            State[chat_id].update(
+                {
+                    "last_chat": tool_name,
+                    "last_request": int(time.time()),
+                    "session_id": None,
+                    "history": [],
+                }
+            )
+
+            await call.message.answer_sticker(tool.get_initial_sticker())
+            return await call.message.answer(
+                tool.get_greeting_text(call.message.chat.username)
+            )
+
+        except Exception:
+            return await call.message.answer(
+                "You need a premium account to have an access to this persona"
+            )
+
+    except Exception:
+        return await call.message.answer(
+            text="something happen please came back letter"
+        )
