504ca74deba842775efe38c0ba5d59717d4c4973
abelkm99 <abelkidanemariam99@gmail.com>
2023-08-25 17:32:07 +0300

    swapped the position of chat and ask

--- afro-chat-backend/bot/features/menu/keyboards.py
+++ afro-chat-backend/bot/features/menu/keyboards.py
@@ -2,8 +2,8 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 start_kb = InlineKeyboardMarkup(row_width=2)
-start_kb.insert(InlineKeyboardButton(text='🗣 AskMe', callback_data='ask'))
 start_kb.insert(InlineKeyboardButton(text='🗯 Chat GPT', callback_data='chat'))
+start_kb.insert(InlineKeyboardButton(text='🗣 AskMe', callback_data='ask'))
 start_kb.insert(InlineKeyboardButton(text='🧑‍🎄 Personas 👥', callback_data='personas'))
 start_kb.insert(InlineKeyboardButton(text='✍️  Tools ', callback_data='tools'))
 # start_kb.insert(InlineKeyboardButton(text='⚙️ Settings', callback_data='setting'))
